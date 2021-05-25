7a80ccee488658a6440379e0f141caf78b414cd2
Software-Engineer-Dima <84675940+Software-Engineer-Dima@users.noreply.github.com>
2021-05-25 20:51:30 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+
+$(document).ready(function() {
+	$('.burger').click(function(event) {
+		$('.burger,.header__burger').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+$(document).ready(function() {
+	$('.footer__body_row-767px__burger').click(function(event) {
+		$('.footer__body_row-767px__burger,.footer__burger,.footer__line-767px').toggleClass('activate');
+		$('body').toggleClass('lock');
+	});
+});
